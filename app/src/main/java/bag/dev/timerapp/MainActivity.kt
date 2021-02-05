2bca6a01a9ff0ffc09f8558fcb77e78379bc1703
bag911 <bagish3000@gmail.com>
2021-02-05 15:14:58 +0300

    timer added

--- app/src/main/java/bag/dev/timerapp/MainActivity.kt
+++ app/src/main/java/bag/dev/timerapp/MainActivity.kt
@@ -3,9 +3,11 @@ package bag.dev.timerapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import bag.dev.timerapp.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -15,7 +17,42 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initView() {
-        binding.progressBar.stopNestedScroll()
+
+//        binding.progressBar2.progress
+        var isRun = false
+
+        var hours:Int
+        var minutes:Int
+        var seconds:Int
+
+        binding.button.setOnClickListener {
+            if (!isRun){
+                var time = 20
+                isRun=true
+                val timer = Timer()
+                timer.schedule(object : TimerTask(){
+                    override fun run(){
+                        runOnUiThread{
+                            time--
+                            hours = time/3600
+                            minutes = time%3600/60
+                            seconds = time%60
+                            binding.textView.text = "$hours : $minutes : $seconds"
+                            if (time==0){
+                                timer.cancel()
+                                isRun = false
+                            }
+//                            binding.progressBar2.progress++
+//                            if (binding.progressBar2.progress==100){
+//                                binding.progressBar2.progress=0
+//                            }
+                        }
+                    }
+                },0,1000)
+            }
+        }
+
+
     }
 
 
